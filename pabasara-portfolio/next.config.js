18d78f3a5b23abd5f5753bb54273aee0e0d1ffbf
PabasaraIlankoon <pvillankoon@gmail.com>
2026-07-02 03:21:38 +0530

    Remove accidental pabasara-portfolio folder

--- pabasara-portfolio/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  output: "export", // for static deploy to GitHub Pages / Vercel
-};
-
-module.exports = nextConfig;
